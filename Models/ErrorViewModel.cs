f3ed139cd79520236d2da7652492eef4b54ca40e
MargaritaSotoRodriguez <margarita.soto@ues.mx>
2024-08-28 13:53:02 -0700

    Crear Proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaVentasV6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
